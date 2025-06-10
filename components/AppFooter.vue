4d1e135c507d4d9b2e7ab824e6321696447205fe
stefanfrancis93 <stefan.mec.93@gmail.com>
2025-06-10 13:42:54 +0900

    first commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <p>&copy; 2025 My Application. All rights reserved.</p>
+    <nav>
+      <ul>
+        <li><a href="/privacy">Privacy Policy</a></li>
+        <li><a href="/terms">Terms of Service</a></li>
+      </ul>
+    </nav>
+  </footer>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <h1>My Application Header</h1>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
